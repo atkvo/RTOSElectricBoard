6558f26f2923fc1f95e8734bc04ebe63b00fa91f
Andrew Vo <voandrew.khoa@gmail.com>
2017-05-16 16:30:18 -0700

    Add basic struct for LCD display

---
+++ L5_Application/OnScreenData.hpp
@@ -0,0 +1,14 @@
+#ifndef L5_APPLICATION_REMOTECONTROL_HPP_
+#define L5_APPLICATION_REMOTECONTROL_HPP_
+
+struct OnScreenData
+{
+    bool connectionStatus;
+    float powerLevel;
+    float batteryLevel;
+    bool eBreakActive;
+};
+
+#endif /* L5_APPLICATION_REMOTECONTROL_HPP_ */
+
+
